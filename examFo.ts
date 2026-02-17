9e2492023aedf91e6d5e657d07cc7b5db8e5d65a
hosodaythemhocthem-blip <hosodaythemhocthem@gmail.com>
2026-02-17 16:43:13 +0700

    Update examFo.ts

--- examFo.ts
+++ examFo.ts
@@ -1,3 +1,29 @@
+export type QuestionType = "MCQ" | "TRUE_FALSE" | "SHORT";
 
-// Chuyển hướng hoàn toàn về types.ts để đồng bộ 1 nguồn duy nhất
-export * from './types';
+export interface BaseQuestion {
+  id: string;
+  content: string;
+  points: number;
+  type: QuestionType;
+}
+
+export interface MCQQuestion extends BaseQuestion {
+  type: "MCQ";
+  options: string[];
+  correct_answer: number;
+}
+
+export interface TrueFalseQuestion extends BaseQuestion {
+  type: "TRUE_FALSE";
+  correct_answer: boolean;
+}
+
+export interface ShortAnswerQuestion extends BaseQuestion {
+  type: "SHORT";
+  correct_answer: string;
+}
+
+export type ExamQuestion =
+  | MCQQuestion
+  | TrueFalseQuestion
+  | ShortAnswerQuestion;
